abcf56b460bd26895faf3b30bd746a53feb79648
Anna Vitória <annavitoria12.mendes05@gmail.com>
2018-10-04 18:04:47 -0300

    Criando operação de instanticação

--- src/main/java/com/projeto/vitoria/VitoriaApplication.java
+++ src/main/java/com/projeto/vitoria/VitoriaApplication.java
@@ -1,12 +1,31 @@
 package com.projeto.vitoria;
 
+import com.projeto.vitoria.domain.Categoria;
+import com.projeto.vitoria.repositorys.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class VitoriaApplication {
+public class VitoriaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VitoriaApplication.class, args);
+	}
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "informática");
+		Categoria cat2 = new Categoria(null, "enfermagem");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+
 	}
 }
