5aa2933908ee31e942022fe075fede07f85c6cac
Anna Vitória <annavitoria12.mendes05@gmail.com>
2018-10-04 16:21:06 -0300

    Projeto Criado

---
+++ src/main/java/com/projeto/vitoria/VitoriaApplication.java
@@ -0,0 +1,12 @@
+package com.projeto.vitoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitoriaApplication.class, args);
+	}
+}
