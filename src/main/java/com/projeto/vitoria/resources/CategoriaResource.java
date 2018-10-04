83de2d32886a3aa06c1cb2e1813d801e520cee2d
Anna Vitória <annavitoria12.mendes05@gmail.com>
2018-10-04 16:57:30 -0300

    adicionado lista json

--- src/main/java/com/projeto/vitoria/resources/CategoriaResource.java
+++ src/main/java/com/projeto/vitoria/resources/CategoriaResource.java
@@ -1,15 +1,25 @@
 package com.projeto.vitoria.resources;
 
+import com.projeto.vitoria.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST ESTA FUNCIONANDO";
+    public List<Categoria> listar(){
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Enfermagem");
+
+        List<Categoria> lista = new ArrayList();
+        lista.add(cat1);
+        lista.add(cat2);
+        return lista;
     }
 }
