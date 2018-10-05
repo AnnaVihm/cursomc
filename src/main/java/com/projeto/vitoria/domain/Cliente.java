f13ed25568c270c2d2b279ace5a4316aae11a836
Anna Vitória <annavitoria12.mendes05@gmail.com>
2018-10-05 10:29:37 -0300

    adicionado classes Estado e Cliente

---
+++ src/main/java/com/projeto/vitoria/domain/Cliente.java
@@ -0,0 +1,99 @@
+package com.projeto.vitoria.domain;
+
+import com.projeto.vitoria.enums.TipoCliente;
+
+import java.util.*;
+
+public class Cliente {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String nome;
+    private String email;
+    private String cpfOuCnpj;
+    private Integer tipoCliente;
+
+    private List<Endereco> enderecos = new ArrayList<>();
+
+    private Set<String> telefone = new HashSet<>();
+
+    public Cliente(){}
+
+    public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipoCliente) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpfOuCnpj = cpfOuCnpj;
+        this.tipoCliente = tipoCliente.getCod();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpfOuCnpj() {
+        return cpfOuCnpj;
+    }
+
+    public void setCpfOuCnpj(String cpfOuCnpj) {
+        this.cpfOuCnpj = cpfOuCnpj;
+    }
+
+    public TipoCliente getTipoCliente() {
+        return TipoCliente.toEnum(tipoCliente);
+    }
+
+    public void setTipoCliente(TipoCliente tipoCliente) {
+        this.tipoCliente = tipoCliente.getCod();
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public Set<String> getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Set<String> telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+}
